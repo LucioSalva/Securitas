3e2a339e5163aed07e27a3f7bb14827745d085c7
Lucio <lucio.s.isc@gmail.com>
2024-10-14 22:54:56 -0600

    se integro el repo local

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { E404Component } from './components/e404/e404.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component:HomeComponent },
+  {path: 'E404', component:E404Component },
+  {path: '**', redirectTo:'/E404', pathMatch:'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
